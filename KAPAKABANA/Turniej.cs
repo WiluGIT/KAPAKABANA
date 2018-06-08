c8b0a85ec44c29409f7da8fa3a24ff11815bf0d6
Michał Zaorski <michal.zaorski98@gmail.com>
2018-06-08 18:48:41 +0200

    dodano klasę Turniej

---
+++ KAPAKABANA/Turniej.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PO_KAPAKABANA;
+
+namespace KAPAKABANA
+{
+
+    class Turniej
+    {
+        private List<Mecz> lista_meczy = new List<Mecz>();
+        private enum typTurnieju { PrzeciaganieLiny, DwaOgnie, Siatkowka };
+        private int id;
+        private Druzyna[] finalisci;
+
+        public Turniej(int idd)
+        {
+            this.id = idd;
+        }
+
+        public void DodajDruzyne()
+        {
+
+        }
+        public void DodajSedziego()
+        {
+
+        }
+
+        public void UsunDruzyne(int idd)
+        {
+
+        }
+
+        public void UsunSedziego(int idd)
+        {
+
+        }
+
+        public void PrzegladDruzyn()
+        {
+
+        }
+
+        public void PrzegladSedziow()
+        {
+
+        }
+
+        public void StworzMecz()
+        {
+
+        }
+
+        public void WypiszWyniki(List<Druzyna> lista_druzyn)
+        {
+
+        }
+
+        public void GenerowanieFinalow()
+        {
+
+        }
+
+        public void ZapisDoPliku(String nazwa)
+        {
+
+        }
+
+        public void OdczytZPliku(String nazwa)
+        {
+
+        }
+
+        public Druzyna[] WyborFinalistow(List<Druzyna> lista_druzyn)
+        {
+        
+            return finalisci;
+        }
+
+
+    }
+}
